1be6d027e2e447cadf064214c9d232e88685dba5
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2025-02-04 15:31:15 +0000

    started adapting code to search by ID - work in progress

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
-const https = require('https');
-const { App } = require('@slack/bolt');
+require("dotenv").config();
+const https = require("https");
+const { App } = require("@slack/bolt");
+const { channel } = require("diagnostics_channel");
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
@@ -14,41 +15,39 @@ const app = new App({
   // Start your app
   await app.start(process.env.PORT || 3000);
 
-  app.logger.info('⚡️ Bolt app is running!');
+  app.logger.info("⚡️ Bolt app is running!");
 
   // Optionally, you can call `findConversation` here or on specific events
-  await findConversation("wellbot");
+  await findConversation("C089K7Q25BR");
 })();
 
 // Find conversation ID using the conversations.list method
-async function findConversation(name) {
+async function findConversation(conversationId) {
   try {
     // Call the conversations.list method using the built-in WebClient
     const result = await app.client.conversations.list({
       // The token you used to initialize your app
-      token: process.env.SLACK_BOT_TOKEN
+      token: process.env.SLACK_BOT_TOKEN,
     });
+    console.log("Fetched conversation" + result.channels[50].name);
 
-    let conversationId = null;
+    // let conversationId = C089K7Q25BR;
 
     // Look through the channels to find the one with the matching name
     for (const channel of result.channels) {
-      if (channel.name === name) {
-        conversationId = channel.id;
-
+      if (conversationId === channel.id) {
         // Print result
-        console.log("Found conversation ID: " + conversationId);
+        console.log("Found conversation: " + channel.name);
         break; // Exit the loop once found
       }
     }
 
     if (!conversationId) {
-      console.log(`No conversation found with the name: ${name}`);
+      console.log(`No conversation found with the ID: ${conversationId}`);
     }
 
-    return conversationId;
-  }
-  catch (error) {
-    console.error('Error fetching conversation:', error);
+    return channel.name;
+  } catch (error) {
+    console.error("Error fetching conversation:", error);
   }
 }
